a1fe01c05dab97f7b823667e19cf8a334d5d2bfc
sulishijian <1097867226@qq.com>
2021-11-04 15:15:46 +0800

    first commit

---
+++ src/main/java/com/sulishijian/Test.java
@@ -0,0 +1,11 @@
+package com.sulishijian;
+
+/**
+ * @author sulishijian
+ * @date 2021/11/4-15:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
